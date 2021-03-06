2c36f74f11c7266452ccbf4716f3f4751f33135d
William Lindvall <shwilliam@hey.com>
2021-03-06 14:08:35 +0100

    textarea value to encoded search param

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
 import './styles.css'
 
-alert('boop')
+const encode = (text: string) => btoa(unescape(encodeURIComponent(text)))
+const decode = (text: string) => decodeURIComponent(escape(atob(text)))
+
+document.addEventListener('DOMContentLoaded', () => {
+  const searchParams = (new URL(String(document.location))).searchParams;
+  const existingText = searchParams.get('t')
+  const textareaEl = document.getElementById('textarea') as HTMLTextAreaElement
+  const copyEl = document.getElementById('copy') as HTMLButtonElement
+
+  if (existingText) {
+    const decodedText = decode(existingText)
+    textareaEl.value = decodedText
+  }
+
+  textareaEl?.addEventListener('keyup', () => {
+    const { value } = textareaEl
+    const encodedText = encode(value)
+
+    searchParams.set('t', encodedText)
+    history.replaceState({}, '', `${location.pathname}?${searchParams}`);
+  })
+
+  copyEl?.addEventListener('click', () => {
+    if (!navigator.clipboard) alert('clipboard not available')
+    navigator.clipboard.writeText(String(location))
+  })
+}, false)

42d65bbd59677a88c90b29da16ab4381df80913f
William Lindvall <shwilliam@hey.com>
2021-03-06 19:18:35 +0100

    placeholder and cleanup

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,26 @@ import './styles.css'
 const qrURL = (url: string) => `https://zxing.org/w/chart?cht=qr&chs=200x200&chld=L&choe=UTF-8&chl=${url}`
 
 document.addEventListener('DOMContentLoaded', () => {
-  const textareaEl = document.getElementById('textarea') as HTMLTextAreaElement
-  const copyEl = document.getElementById('copy') as HTMLButtonElement
-  const qrEl = document.getElementById('qr') as HTMLAnchorElement
-  const searchParams = (new URL(String(document.location))).searchParams
-  const existingText = searchParams.get('t')
+  const textareaEl = document.getElementById('textarea') as HTMLTextAreaElement | undefined,
+    copyEl = document.getElementById('copy') as HTMLButtonElement | undefined,
+    qrEl = document.getElementById('qr') as HTMLAnchorElement | undefined,
+    searchParams = (new URL(String(document.location))).searchParams,
+    existingText = searchParams.get('t')
 
-  if (existingText) {
+  if (existingText && textareaEl && qrEl) {
     const decodedText = decompressFromBase64(existingText)
     textareaEl.value = decodedText ?? ''
     qrEl.href = qrURL(location.href)
   }
 
   textareaEl?.addEventListener('keyup', () => {
-    const { value } = textareaEl
-    const encodedText = compressToBase64(value)
-    const newURL = `${location.pathname}?${searchParams}`
+    const { value } = textareaEl,
+      encodedText = compressToBase64(value),
+      newURL = `${location.pathname}?${searchParams}`
 
     searchParams.set('t', encodedText)
     history.replaceState({}, '', newURL)
-    qrEl.href = qrURL(`${location.origin}${newURL}`)
+    if (qrEl) qrEl.href = qrURL(`${location.origin}${newURL}`)
   })
 
   copyEl?.addEventListener('click', () => {

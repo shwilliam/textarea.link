9f01383fa25aa1002f7b819bf9d2843dc56c36d6
William Lindvall <shwilliam@hey.com>
2021-03-09 01:39:51 +0100

    require post password

--- migrations/20210307204157_create_posts.js
+++ migrations/20210307204157_create_posts.js
@@ -1,9 +1,9 @@
 exports.up = function (knex) {
   return knex.schema.createTable('posts', function (table) {
     table.increments('id')
-    table.string('title', 255).notNullable()
+    table.string('title', 255)
     table.text('body')
-    table.string('password', 255)
+    table.string('password', 255).notNullable()
   })
 }
 
